7bb621ed5185acee77993f4b9ba4291c5353ab91
huzixi733-code <huzixi733@gmail.com>
2026-09-13 18:11:33 +0800

    Add sw.js

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='shici-cet6-v2',ASSETS=['./','./index.html','./styles.css?v=2','./app.js?v=2','./learn.js?v=2','./manifest.webmanifest','./favicon.svg'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;if(e.request.mode==='navigate'){e.respondWith(fetch(e.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put('./index.html',copy));return res}).catch(()=>caches.match('./index.html')));return}e.respondWith(caches.match(e.request).then(hit=>hit||fetch(e.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return res}))) });
+
